337d744d11b5005446c6a8a0392ea8292420d3d0
kornel <gg@g.k>
2025-09-08 13:48:20 +0200

    dodanie imageview, datepicker, spinner

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.test
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val imageMap = mapOf(
+        "Człowiek" to R.drawable.sword,
+        "Elf" to R.drawable.bow,
+        "Hobbit" to R.drawable.ring,
+        "Czarodziej" to R.drawable.wand,
+        "Krasnolud" to R.drawable.axe
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +29,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val spinner: Spinner = findViewById(R.id.rasa)
+        val imageView: ImageView = findViewById(R.id.zdj)
+        val rasy = arrayOf("Człowiek", "Elf", "Hobbit", "Czarodziej", "Krasnolud")
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, rasy
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent?.getItemAtPosition(position) as String
+                val imageResId = imageMap[selectedItem]
+                if (imageResId != null){
+                    imageView.setImageResource(imageResId)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                imageView.setImageResource(0)
+            }
         }
     }
 }
